a0345f90743049197e965634adb35013433cdf2a
sjsm <wonner_cw@163.com>
2016-08-23 10:08:51 +0800

    Initial commit

---
+++ app/src/main/java/com/example/sjsm/myapplication/MainActivity.java
@@ -0,0 +1,98 @@
+package com.example.sjsm.myapplication;
+
+import android.content.pm.ApplicationInfo;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private List<ApplicationInfo> mAppList;
+    private DragDelListView mListView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mAppList = getPackageManager().getInstalledApplications(0);
+        mListView = (DragDelListView) findViewById(R.id.listView);
+        mListView.setAdapter(new AppAdapter(mAppList));
+    }
+    class AppAdapter extends BaseAdapter {
+        private List<ApplicationInfo> mAppList;
+        public AppAdapter(List<ApplicationInfo> appList)
+        {
+            mAppList=appList;
+        }
+        @Override
+        public int getCount() {
+            return mAppList.size();
+        }
+
+        @Override
+        public ApplicationInfo getItem(int position) {
+            return mAppList.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+
+            final int loc=position;
+            ViewHolder holder=null;
+            View menuView=null;
+            if (convertView == null) {
+                convertView = View.inflate(getApplicationContext(),
+                        R.layout.swipecontent, null);
+                menuView = View.inflate(getApplicationContext(),
+                        R.layout.swipemenu, null);
+                //合成内容与菜单
+                convertView = new DragDelItem(convertView,menuView);;
+                holder=new ViewHolder(convertView);
+            } else {
+                holder = (ViewHolder) convertView.getTag();
+            }
+            ApplicationInfo item = getItem(position);
+            holder.iv_icon.setImageResource(R.mipmap.ic_launcher);
+            holder.tv_name.setText(item.loadLabel(getPackageManager()));
+            holder.tv_open.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View arg0) {
+                    Toast.makeText(MainActivity.this, "open:"+loc, Toast.LENGTH_SHORT).show();
+                }
+            });
+            holder.tv_del.setOnClickListener(new View.OnClickListener() {
+
+                @Override
+                public void onClick(View arg0) {
+
+                }
+            });
+            return convertView;
+        }
+
+        class ViewHolder {
+            ImageView iv_icon;
+            TextView tv_name;
+            TextView tv_open,tv_del;
+            public ViewHolder(View view) {
+                iv_icon = (ImageView) view.findViewById(R.id.iv_icon);
+                tv_name = (TextView) view.findViewById(R.id.tv_name);
+                tv_open=(TextView)view.findViewById(R.id.tv_open);
+                tv_del=(TextView)view.findViewById(R.id.tv_del);
+                view.setTag(this);
+            }
+        }
+    }
+}
